0197f03c6a635d9f0ad157067c85058fa5d71df8
fjgomez <fjgomez@PFJGOMEZ.florida.local>
2016-10-20 16:49:31 +0200

    Primer proyecto

---
+++ src/primerPrograma/HolaPaco.java
@@ -0,0 +1,10 @@
+package primerPrograma;
+
+public class HolaPaco {
+
+	public static void main(String[] args) {
+		// Voy a imprimir algo por pantalla
+		System.out.println("HOLA PACO ERES UN CACHONDO!!!");
+	}
+
+}
