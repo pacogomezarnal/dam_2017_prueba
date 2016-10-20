2158903e567f510c2fa476d74b9e840a325bd659
fjgomez <fjgomez@PFJGOMEZ.florida.local>
2016-10-20 16:50:20 +0200

    Cambiado HolaPaco

--- src/primerPrograma/HolaPaco.java
+++ src/primerPrograma/HolaPaco.java
@@ -3,7 +3,7 @@ package primerPrograma;
 public class HolaPaco {
 
 	public static void main(String[] args) {
-		// Voy a imprimir algo por pantalla
+		// Voy a imprimir algo por pantalla 2
 		System.out.println("HOLA PACO ERES UN CACHONDO!!!");
 	}
 
